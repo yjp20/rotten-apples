958e74806aacee5c11cb192cb1d4255eacb1ab2e
Ryan Diep <ryan.m.diep@gmail.com>
2022-10-16 11:31:52 -0700

    Implement summary and multithreading

--- scripts/process_files.py
+++ scripts/process_files.py
@@ -16,17 +16,23 @@ import sys
 
 api_key = "bVBDxyqnmSkyQvWF8JTAm4nb0szv5Cy5gyivcysD"
 co = cohere.Client(api_key)
-examples = [Example("", "")]
-inputs = ["aaaa",
-    "bbbb"]
+examples = []
+inputs = []
    
+
+#summarize all csv files
+#take in example data into examples list
+#run classifier for each csv file, clear inputs
+#done
+
 def read_files():
     for urmom in sys.argv[1:]:
             csv_functions.applyReview(urmom, urmom, csv_functions.summarizer, "summarized")
-            csv_functions.applyReview(urmom, urmom, csv_functions.classifier, "classifier")
-
-def read_files_individual():
-    csv_functions.applyReview(sys.argv[1], sys.argv[1], csv_functions.summarizer, "summarized")
-    #csv_functions.applyReview(sys.argv[0], sys.argv[0], csv_functions.summarizer, "classifier")
-
+            #csv_functions.applyReview(urmom, urmom, csv_functions.classifier, "classifier")
+    df = pd.read_csv("reviews.csv")
+    for index, row in df.iterrows():
+        #print(index)
+        examples.append(Example(row['review_rating'], row['summarized']))
+    print(examples)
+        
 read_files()
