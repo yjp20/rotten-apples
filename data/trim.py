e7e38a29bf209022ba727811e373509fea9e6bd4
Ryan Diep <ryan.m.diep@gmail.com>
2022-10-16 06:23:37 -0700

    im an idiot

--- data/trim.py
+++ data/trim.py
@@ -14,7 +14,7 @@ def trim_files():
 
 #catenate the trimmed files into single csv
 def cat_trimmed():
-    combined_csv = pd.concat([pd.read_csv(file) for file in sys.argv[1:])
+    combined_csv = pd.concat([pd.read_csv(file) for file in sys.argv[1:]])
     combined_csv.to_csv("finetuning.csv", index=False, encoding='utf-8-sig')
     
 trim_files()
